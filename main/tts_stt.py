c78a81bc30b1e21f541224df38bd94eb0b5aa6af
Steven Petrov <91495882+StevenPetrov@users.noreply.github.com>
2022-08-29 01:11:52 +0300

    added text to speech and speech to text
    tts_stt.py

---
+++ main/tts_stt.py
@@ -0,0 +1,36 @@
+import pyttsx3
+import speech_recognition as sr
+
+
+def text_to_speach(text):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 120)
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    engine.say(text)
+    engine.runAndWait()
+
+
+# text_to_speach('Hello')
+# text_to_speach('This is your home assistant in Grigorevo')
+
+
+def speech_to_text():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        # text_to_speach("Speak Anything")
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio)
+            print("You said : {}".format(text))
+            return text
+        except:
+            print("Sorry could not recognize what you said")
+
+
+while True:
+    command = speech_to_text()
+    if command is not None:
+        if 'hello' in command and 'Jarvis' in command:
+            text_to_speach('I am ready for your commands')
+            break
